d2640b6a001411189e760b1256050cfc5f8bd6b8
rkorostin <rkorostin@gmail.com>
2022-12-18 11:45:49 +0300

    Task6. Сумма цифр в вещественном числе. Поправка от преподавателя

--- 295349/task6.py
+++ 295349/task6.py
@@ -7,15 +7,18 @@
 - 0,56 -> 11
 """
 
+num = input("Введите число:")  # Получаем данные типа string
 
-num = input("Введите число:") # Получаем данные типа string
+# if '-' in num:
+#     num = num.replace('-', '')  # На случай, если ввели отрицательное число
 
-if '-' in num:
-    num = num.replace('-','') # На случай, если ввели отрицательное число
+for symbol in ['-', '.']:
+    if symbol in num:
+        num = num.replace(symbol, "")
 
-num_revers = num[::-1] # Переворачиваем наоборот введённое значение
+num_revers = num[::-1]  # Переворачиваем наоборот введённое значение
 
-num = float(num_revers) # Преобразуем во float
+num = float(num_revers)  # Преобразуем во float
 
 sumNumbers = 0
 while num > 0:  # Для подсчета суммы цифр в заданном числе берем операции деления и взятия остатка
